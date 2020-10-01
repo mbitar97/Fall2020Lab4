182b28b3531febf084b5d530dac29d0d91eaa833
Mariam <mbit@umich.edu>
2020-10-01 17:05:48 -0400

    changed loop to 12

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 public class Hello{
   public static void main(string[] args){
-    for (int i=1; i<=11; i++)
+    for (int i=1; i<=12; i++)
     {
       System.out.println("Hello World ... " + i);
     }
