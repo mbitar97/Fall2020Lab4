7f8a2e0349cf8e4b2bc5ab4e1091988d4e039af4
Mariam <mbit@umich.edu>
2020-10-01 18:48:03 -0400

    changed

--- Hello.java
+++ Hello.java
@@ -1,5 +1,5 @@
 public class Hello{
-  public static void main(string[] args){
+  public static void main(String[] args){
     for (int i=1; i<=12; i++)
     {
       System.out.println("Hello World ... " + i);
